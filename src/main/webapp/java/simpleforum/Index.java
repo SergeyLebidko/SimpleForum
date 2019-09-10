1798f62347d3ce6a8da056ba34adb30af2644303
SergeyLebidko <it.sergeyler@mail.ru>
2019-09-10 20:27:25 +0300

    Добавлен код создания сессии

--- src/main/webapp/java/simpleforum/Index.java
+++ src/main/webapp/java/simpleforum/Index.java
@@ -1,9 +1,12 @@
 package simpleforum;
 
+import simpleforum.dao.Account;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,9 +17,18 @@ public class Index extends HttpServlet {
         resp.setCharacterEncoding("Windows-1251");
         resp.setContentType("text/html");
 
+        HttpSession session = req.getSession(false);
+        Account account = null;
+        if (session != null) {
+            account = (Account) session.getAttribute("login_user");
+        }
+
         PrintWriter out = resp.getWriter();
 
         out.print("<h3>Главная страница</h3>");
+        if (account != null) {
+            out.print("<br>Вошёл: " + account.getUsername());
+        }
     }
 
 }
--- src/main/webapp/java/simpleforum/Login.java
+++ src/main/webapp/java/simpleforum/Login.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -54,6 +55,8 @@ public class Login extends HttpServlet {
             resp.sendRedirect("login");
             return;
         }
+        HttpSession session = req.getSession(true);
+        session.setAttribute("login_user", account);
         resp.sendRedirect("index");
     }
 
